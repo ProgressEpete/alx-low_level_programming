45a2f076e46a95345ad31607de49755192b5c38a
ProgressEpete <epeteprogress>
2022-07-14 13:25:45 -0700

    5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
 **/
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora");
+fprintf(stderr, "Korpar, 2015-10-19\n");
 return (1);
 }
