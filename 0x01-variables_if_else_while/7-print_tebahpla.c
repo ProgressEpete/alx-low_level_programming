c1bf9b587be0a911675b25deb452c5fbb389da7e
progress epete <epeteprogress@gmail.com>
2022-07-18 03:39:13 -0700

    5

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
-sbeishe
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main -> Write a program that prints the lowercase alphabet in reverse
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+	putchar(ch);
+
+	putchar(10); /*this is an ascii code for newline*/
+
+	return (0);
+}
