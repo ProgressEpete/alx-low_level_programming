d5552fd678bf949b2bb1da8dead28ea1d39e05f3
progress epete <epeteprogress@gmail.com>
2022-07-18 04:11:59 -0700

    2

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - Write a program that prints all possible
+ * combinations of single-digit numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+for (ch = 48; ch <= 57; ch++)
+{
+	putchar(ch);
+	if (ch != 57)
+	{
+
+	putchar(44);
+	putchar(32);
+	}
+}
+
+	putchar(10); /*this is an ascii code for newline*/
+
+	return (0);
+}
