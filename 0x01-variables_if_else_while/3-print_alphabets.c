27ad834e12d2a954c3646f06624a744e1e3fbd70
progress epete <epeteprogress@gmail.com>
2022-07-18 07:15:26 -0700

    1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,13 @@ int main(void)
 	int ch;
 
 	for (ch = 97; ch <= 122; ch++)
-	{
+{
 	putchar(ch);
-	}
+}
+	for (ch = 65; ch <= 90; ch++)
+{
+	putchar(ch);
+}
 	putchar(10); /*this is an ASCII code for newline*/
 	return (0);
 }
