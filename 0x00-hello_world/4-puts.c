59ccc031b279811e2813d60a4c2411e63e881e80
Progress Epete <epeteprogress@gmail.com>
2022-10-13 15:40:46 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main functions
+ * return (0)
+ * Write a C program that prints exactly Programming is like
+ * building a multilingual puzzle, followed by a new line.
+ */
+
+	int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
