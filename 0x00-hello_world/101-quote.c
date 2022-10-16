52be36b23bd81d0cee00ef0e42a0460f705e03d6
Progress Epete <epeteprogress@gmail.com>
2022-10-16 12:09:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
-* main- Entry point
-* @void: parameter
-* Return: 1 (success)
+* main - print quote
+* Description: prints Dora's quote
+* Return: Always 1 (Success)
 */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
